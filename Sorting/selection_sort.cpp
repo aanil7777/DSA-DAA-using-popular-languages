c551254a1fc121f60d6fddd3d61143ee24e8f9f7
Ashish <44299689+aanil7777@users.noreply.github.com>
2019-10-01 08:43:28 +0530

    Update selection_sort.cpp

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -1,35 +1,47 @@
-/* C++ Program - Selection Sort */
-		
-#include<iostream.h>
-#include<conio.h>
-void main()
-{
-	clrscr();
-	int size, arr[50], i, j, temp;
-	cout<<"Enter Array Size : ";
-	cin>>size;
-	cout<<"Enter Array Elements : ";
-	for(i=0; i<size; i++)
-	{
-		cin>>arr[i];
-	}
-	cout<<"Sorting array using selection sort...\n";
-	for(i=0; i<size; i++)
-	{
-		for(j=i+1; j<size; j++)
-		{
-			if(arr[i]>arr[j])
-			{
-				temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
-			}
-		}
-	}
-	cout<<"Now the Array after sorting is :\n";
-	for(i=0; i<size; i++)
-	{
-		cout<<arr[i]<<" ";
-	}
-	getch();
-}
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+void swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+}  
+  
+void selectionSort(int arr[], int n)  
+{  
+    int i, j, min_idx;  
+  
+    // One by one move boundary of unsorted subarray  
+    for (i = 0; i < n-1; i++)  
+    {  
+        // Find the minimum element in unsorted array  
+        min_idx = i;  
+        for (j = i+1; j < n; j++)  
+        if (arr[j] < arr[min_idx])  
+            min_idx = j;  
+  
+        // Swap the found minimum element with the first element  
+        swap(&arr[min_idx], &arr[i]);  
+    }  
+}  
+  
+/* Function to print an array */
+void printArray(int arr[], int size)  
+{  
+    int i;  
+    for (i=0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}  
+  
+// Driver program to test above functions  
+int main()  
+{  
+    int arr[] = {64, 25, 12, 22, 11};  
+    int n = sizeof(arr)/sizeof(arr[0]);  
+    selectionSort(arr, n);  
+    cout << "Sorted array: \n";  
+    printArray(arr, n);  
+    return 0;  
+}  
